770fa5215717bfca383fe6a6dd241618d27ae75a
diya26322632 <138868720+diya26322632@users.noreply.github.com>
2023-08-30 22:23:06 +0530

    Update EVM_BEGINNER.sol

--- EVM_BEGINNER.sol
+++ EVM_BEGINNER.sol
@@ -1,44 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
-contract MyToken {
+contract BTC_Token {
 
-    // Public variables
     string public tokenName = "Bitcoin";
     string public Abbrv = "Btc";
     uint public totalSupply = 10;
 
-    // Mapping of addresses to balances
     mapping(address => uint) public balances;
 
-    // Owner address
     address public owner;
 
-    // Constructor to set the owner
     constructor() {
         owner = msg.sender;
     }
 
-    // Modifier to ensure only the owner can execute certain functions
+    
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 
-    // Mint function
+    // Mint function 
     function mintBtcToken(address _address, uint _value) public onlyOwner {
         totalSupply += _value;
         balances[_address] += _value;
     }
 
-    // Burn function
+    // Burn function 
     function burnBtcToken(address _address, uint _value) public onlyOwner {
         require(balances[_address] >= _value, "Insufficient balance");
         totalSupply -= _value;
         balances[_address] -= _value;
     }
-
-    // Function to get the owner's address
+    
     function getOwner() public view returns (address) {
         return owner;
     }
